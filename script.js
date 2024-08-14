1af5d0d4233f6aaf652d696506cbe66c95f97260
Sudip Mahata <imsudipmahata@gmail.com>
2024-08-14 20:54:14 +0530

    updated javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+const spinnerWrapperEl =document.querySelector('.spinner-wrapper');
+
+window.addEventListener('load', () =>{
+    setTimeout(() =>{
+        spinnerWrapperEl.style.display='none';
+    } ,800)
+    spinnerWrapperEl.style.opacity='0';
+});
+
+
+// Toast
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+};
+
+
+let passCode='friendzone';
+let userCode=prompt("Enter PassWord to access this WebPage :");
+
+while(passCode!==userCode){
+    userCode=prompt("Nope that's not the right password! Try again or contact with the developer :")
+}
+console.log("Welcome ! You guessed the right password");
